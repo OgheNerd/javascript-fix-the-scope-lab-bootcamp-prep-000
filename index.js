1b8e9bb8bf7ba1d6d8e20b41a545f9b29996dcf5
Oghene Michael <zonalmeet@gmail.com>
2018-01-27 21:06:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
+  return function innerScope() {
     return "FUNKY!"
   }
 }
@@ -27,4 +27,3 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction();
-
